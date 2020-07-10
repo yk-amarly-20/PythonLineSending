c1a7ec5604e12a84c2989babce7154fc66fc467c
小島悠人 <kojimayuto@mbp.local>
2020-07-10 17:04:21 +0900

    テストコード

---
+++ test/test.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../')
+from MessagingSender import MessagingSender 
+
+def main(token, message, image):
+    sender = MessagingSender(token)
+    sender.send_message(message, image)
+
+    return
+
+if __name__ == '__main__':
+    with open('./token.txt', 'r') as f:
+        token = f.read()[:-1]
+
+    message = 'hello world'
+    image = './11195179127721.jpg'
+
+    main(token, message, image)
